f8ca5c206def12d4799b6f54bb77a59121666c29
Pradipta Bhuin <pradiptabhuin@Pradiptas-MacBook-Pro.local>
2024-12-29 12:42:14 +0530

    pdf gap less

--- resources/views/pdf/pescription.blade.php
+++ resources/views/pdf/pescription.blade.php
@@ -37,7 +37,7 @@
 
     <section class="color-black">
         <div class="bg-white container" style="solid;color:#1356a7;padding:5px;">
-            <div class="row" style="margin-bottom:30px;">
+            <div class="row" style="margin-bottom:0px;">
                 <div class="span-2">
                     <img src="{{asset('pescription/images/logo.png')}}" style="width:150px;">
                 </div>
@@ -56,10 +56,10 @@
                     <p><span style="font-weight:600;color:black;"> 9800517218</span></p>
                 </div>
             </div>
-            <div class="row" style="border:1px #000000 solid;border-radius:30px;padding:30px;color:#000000;">
+            <div class="row" style="border:1px #000000 solid;border-radius:30px;padding:15px 30px;color:#000000;">
                 <div class="span-12">
-                    <p class="three-line"><span>OPD/ORC No<strong class="line3"style="width:200px;">{{ $bookingInfo?->booking_id ?? ' ' }}</strong></span>
-                        <span>Venue<strong class="line3"style="width:100px;">{{ $bookingInfo?->pescription?->venue }}</strong></span><span>Date<strong
+                    <p class="three-line"><span>OPD/ORC No<strong class="line3"style="width:180px;">{{ $bookingInfo?->booking_id ?? ' ' }}</strong></span>
+                        <span>Venue<strong class="line3"style="width:120px;">{{ $bookingInfo?->pescription?->venue }}</strong></span><span>Date<strong
                                 class="line3" style="width:100px;">{{ \Carbon\Carbon::parse($bookingInfo?->created_at)->format('d M, Y h:i A') }}</strong></span></p>
                     <p class="single-line"><span>Pationt's Name</span><strong class="full-line2">{{ $bookingInfo?->patient->name }} </strong></p>
                     <p class="single-line"><span>Father's/Husband Name</span><strong class="full-line2"> {{ $bookingInfo?->pescription?->guardians_name ?? ' ' }}</strong></p>
@@ -71,7 +71,7 @@
                         <span>Mobile<strong class="line3"style="width:150px;">{{ $bookingInfo?->patient->phone_number }}</strong></span></p>
                 </div>
             </div>
-            <div class="row" style="margin-top:30px;">
+            <div class="row" style="margin-top:0px;">
                 <div class="span-6" style="color:#000000;">
                     <div style="border:1px #000000 solid;padding:0;text-align:center;margin-bottom:0;">
                         <p><b>Clinical Findings</b></p>
