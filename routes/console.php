3c9e1f9ca8c2e7be249db158731daf5fdfc16265
Pradipta Bhuin <pradiptabhuin@Pradiptas-MacBook-Pro.local>
2025-01-18 10:58:19 +0530

    test cmd

--- routes/console.php
+++ routes/console.php
@@ -8,8 +8,10 @@ use Illuminate\Support\Facades\Schedule;
 //     $this->comment(Inspiring::quote());
 // })->purpose('Display an inspiring quote')->hourly();
 
-Schedule::command('app:booking-type-wise-save-into-expenditure')->daily();
-// Schedule::command('app:booking-type-wise-save-into-expenditure')->everyFiveSeconds();
+// Schedule::command('app:booking-type-wise-save-into-expenditure')->daily();
+
+
+Schedule::command('app:test-comd')->everyFiveSeconds();
 // $schedule->command('app:booking-type-wise-save-into-expenditure')
 //     ->everyFiveSeconds()
 //     ->appendOutputTo(storage_path('logs/booking-type-wise-save-into-expenditure.log'));
