cc36efae54b37f883d9675217092d5c364e8a373
pradipta-pd <pradipta.bhuin@unifiedinfotech.net>
2025-01-20 18:03:34 +0530

    console added

--- routes/console.php
+++ routes/console.php
@@ -8,10 +8,10 @@ use Illuminate\Support\Facades\Schedule;
 //     $this->comment(Inspiring::quote());
 // })->purpose('Display an inspiring quote')->hourly();
 
-// Schedule::command('app:booking-type-wise-save-into-expenditure')->daily();
+Schedule::command('app:booking-type-wise-save-into-expenditure')->daily();
 
 
-Schedule::command('app:test-comd')->everyFiveSeconds();
+// Schedule::command('app:test-comd')->everyFiveSeconds();
 // $schedule->command('app:booking-type-wise-save-into-expenditure')
 //     ->everyFiveSeconds()
 //     ->appendOutputTo(storage_path('logs/booking-type-wise-save-into-expenditure.log'));
